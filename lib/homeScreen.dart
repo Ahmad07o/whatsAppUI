cf06d7686f8babdc4e6618217278e658ee4814a5
Ahmad07o <aj120262@gmail.com>
2026-02-06 02:23:16 +0500

    this is my third  commit

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -19,13 +19,40 @@ class _HomescreenState extends State<Homescreen> {
             title: Center(
               child: Text('WhatsApp', style: TextStyle(color: Colors.white)),
             ),
+            actions: [
+              Icon(Icons.search),
+              PopupMenuButton(
+                icon: Icon(Icons.more_vert_outlined),
+                itemBuilder: (context) => [
+                  PopupMenuItem(value: 1, child: Text('New Group')),
+                  PopupMenuItem(value: 2, child: Text('Settings')),
+                  PopupMenuItem(value: 3, child: Text('Logout')),
+                ],
+              ),
+              SizedBox(width: 10),
+            ],
             backgroundColor: Colors.teal,
             bottom: TabBar(
               tabs: [
-                Tab(child: Icon(Icons.search)),
-                Text('chats', style: TextStyle(fontSize: 16)),
-                Text('status', style: TextStyle(fontSize: 16)),
-                Text('calls', style: TextStyle(fontSize: 16)),
+                Tab(child: Icon(Icons.camera_alt)),
+                Tab(
+                  child: Text(
+                    'chats',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'status',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'calls',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
               ],
             ),
           ),
@@ -33,9 +60,43 @@ class _HomescreenState extends State<Homescreen> {
           body: TabBarView(
             children: [
               Text('Camera'),
-              Text('chat'),
+              ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://i.etsystatic.com/13924065/r/il/f9f929/5636214818/il_fullxfull.5636214818_r4vi.jpg',
+                      ),
+                    ),
+                    title: Text('Randy ortan'),
+                    subtitle: Text('where is john cena'),
+                    trailing: Text('6:36'),
+                  );
+                },
+              ),
               Text('status'),
-              Text('calls'),
+              ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://i.etsystatic.com/13924065/r/il/f9f929/5636214818/il_fullxfull.5636214818_r4vi.jpg',
+                      ),
+                    ),
+                    title: Text('Randy ortan'),
+                    subtitle: Text(
+                      index / 2 == 0
+                          ? 'you missed call'
+                          : 'you missed video call',
+                    ),
+                    trailing: Icon(
+                      index / 2 == 0 ? Icons.phone : Icons.video_call,
+                    ),
+                  );
+                },
+              ),
             ],
           ),
         ),
